d61a9adb095df2b5330bb791df2617d0b95de3fa
kirtiganbalu <B032310873@student.utem.edu.my>
2026-04-03 10:48:11 +0800

    chassis mainbasic
    chassis mainbasic page
    
    Co-Authored-By: aidazainondin <272690340+aidazainondin@users.noreply.github.com>

--- assets/vendor/bootstrap/js/externalJs/engine/script.js
+++ assets/vendor/bootstrap/js/externalJs/engine/script.js
@@ -1,51 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-    const contentArea = document.getElementById('tempat-kandungan');
-    const sidebarLinks = document.querySelectorAll(".sidebar-menu .sidebar-item");
+document.addEventListener("DOMContentLoaded", function () {
+  const contentArea = document.getElementById("tempat-kandungan");
+  const sidebarLinks = document.querySelectorAll(".sidebar-menu .sidebar-item");
 
-    
-    if (contentArea) {
-        loadContent('../../module/engine/engine.html');
-    }
+  if (contentArea) {
+    loadContent("../../module/chassis/chassis.html");
+  }
 
-    
-    sidebarLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            
-          
-            event.preventDefault(); 
+  sidebarLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
 
-            
-            sidebarLinks.forEach(btn => btn.classList.remove('active'));
-            
-            
-            this.classList.add('active');
+      sidebarLinks.forEach((btn) => btn.classList.remove("active"));
 
-            
-            const targetPage = this.getAttribute('href');
-            
-            
-            loadContent(targetPage);
-        });
+      this.classList.add("active");
+
+      const targetPage = this.getAttribute("href");
+
+      loadContent(targetPage);
     });
+  });
 
-    function loadContent(url) {
-        fetch(url)
-            .then(response => {
-                if (!response.ok) throw new Error("Gagal cari fail");
-                return response.text();
-            })
-            .then(data => {
-              
-                contentArea.innerHTML = data;
-            })
-            .catch(error => {
-                contentArea.innerHTML = "<h2>Harap maaf, kandungan tidak dijumpai.</h2>";
-                console.error('Ralat:', error);
-            });
-    }
+  function loadContent(url) {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) throw new Error("Gagal cari fail");
+        return response.text();
+      })
+      .then((data) => {
+        contentArea.innerHTML = data;
+      })
+      .catch((error) => {
+        contentArea.innerHTML =
+          "<h2>Harap maaf, kandungan tidak dijumpai.</h2>";
+        console.error("Ralat:", error);
+      });
+  }
 });
-
-
-
-
