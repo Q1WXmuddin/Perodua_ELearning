f69d4bd967fb5ee1cdd6c9894694fd8c69b95f6b
kirtiganbalu <B032310873@student.utem.edu.my>
2026-04-06 17:13:06 +0800

    monday updates
    monday codes updates
    
    Co-Authored-By: aidazainondin <272690340+aidazainondin@users.noreply.github.com>

---
+++ assets/vendor/bootstrap/js/externalJs/layout/script_main.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const contentArea = document.getElementById("tempat-kandungan");
+  const sidebarLinks = document.querySelectorAll(".sidebar-menu .sidebar-item");
+
+  if (contentArea) {
+    loadContent("../../module/chassis/definition/definition1.html");
+  }
+
+  sidebarLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      sidebarLinks.forEach((btn) => btn.classList.remove("active"));
+
+      this.classList.add("active");
+
+      const targetPage = this.getAttribute("href");
+
+      loadContent(targetPage);
+    });
+  });
+
+  function loadContent(url) {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) throw new Error("Gagal cari fail");
+        return response.text();
+      })
+      .then((data) => {
+        contentArea.innerHTML = data;
+      })
+      .catch((error) => {
+        contentArea.innerHTML =
+          "<h2>Harap maaf, kandungan tidak dijumpai.</h2>";
+        console.error("Ralat:", error);
+      });
+  }
+});
